448f1f187cef4776ead8a4cb834736b4b187813e
Andreeea Vescan <andreea.vescan@gmail.com>
2019-03-07 08:30:37 +0200

    Initial commit

---
+++ src/main/java/Lecture02MaxPointsParticipantsMV/App.java
@@ -0,0 +1,13 @@
+package Lecture02MaxPointsParticipantsMV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Lecture02MaxPointsParticipantsMV/AppTest.java
@@ -0,0 +1,20 @@
+package Lecture02MaxPointsParticipantsMV;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
